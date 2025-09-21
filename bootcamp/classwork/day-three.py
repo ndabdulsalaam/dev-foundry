af941e1d49d51f726a30fada15cb0b57653d4842
NdAbdulsalaam <ndabdulsalaam@gmail.com>
2025-09-21 10:04:29 +0100

    master decorator

--- bootcamp/classwork/day-three.py
+++ bootcamp/classwork/day-three.py
@@ -1,4 +1,17 @@
-def greet(name):
-    print(f"hello, {name}")
+import time
+from functools import wraps
 
-greet("Kola")
+def timed(fn):
+    @wraps(fn)
+    def wrapper(*args, **kwargs):
+        start_time = time.perf_counter()
+        run_program = fn(*args, **kwargs)
+        end_time = time.perf_counter()
+        print(f"The program took {end_time - start_time:.2f} second(s)")
+    return wrapper
+
+@timed
+def program_time(n: int):
+    time.sleep(n)
+
+program_time("5")
